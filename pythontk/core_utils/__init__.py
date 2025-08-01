12519942ffa4ed707d41befc403fffc77198e7a3
m3trik@live.com <m3trik@live.com>
2025-08-01 19:22:21 -0400

    0.7.27

--- pythontk/core_utils/__init__.py
+++ pythontk/core_utils/__init__.py
@@ -3,7 +3,7 @@
 
 from pythontk.core_utils.help_mixin import HelpMixin  # Before CoreUtils
 from pythontk.core_utils._core_utils import CoreUtils
-from pythontk.core_utils.pkg_manager import PkgManager
+from pythontk.core_utils.pkg_manager import PkgManager, PkgVersionCheck, PkgVersionUtils
 from pythontk.core_utils.class_property import ClassProperty  # Before LoggingMixin
 from pythontk.core_utils.logging_mixin import LoggingMixin
 from pythontk.core_utils.namespace_handler import NamespaceHandler
