9b76fbbaedefe6b08c3e7b65b4b744ccdff60397
m3trik@live.com <m3trik@live.com>
2026-08-02 14:46:08 -0400

    Bump version to 0.9.3 [skip ci]

--- pythontk/__init__.py
+++ pythontk/__init__.py
@@ -3,7 +3,7 @@
 from pythontk.core_utils.module_resolver import bootstrap_package
 
 __package__ = "pythontk"
-__version__ = "0.9.2"
+__version__ = "0.9.3"
 
 """Expose toolkit utilities with explicit resolver include maps for clarity."""
 
