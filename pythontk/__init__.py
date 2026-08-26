dedf64c8616141b3cff72f7bc800308cb7ec3d6a
m3trik@live.com <m3trik@live.com>
2026-08-26 18:34:40 -0400

    Release 0.9.33

--- pythontk/__init__.py
+++ pythontk/__init__.py
@@ -3,7 +3,7 @@
 from pythontk.core_utils.module_resolver import bootstrap_package
 
 __package__ = "pythontk"
-__version__ = "0.9.32"
+__version__ = "0.9.33"
 
 """Expose toolkit utilities with explicit resolver include maps for clarity."""
 
